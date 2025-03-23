0930d91c1397e8b3e91552a7925acf729aeafa6d
chao hong <101560570+github940719@users.noreply.github.com>
2025-03-23 16:49:17 +0800

    Update address.cpp

--- data structure/vscode/address.cpp
+++ data structure/vscode/address.cpp
@@ -8,11 +8,16 @@ int main() {
     int dim[3] = {10, 20, 30} ;
     cout << (&arr[index[0]][index[1]][index[2]] - &arr[0][0][0]) << endl ;
 
+    // O(n) (for future use)
+    int remain[3] = {0, 0, 1} ;
+    for (int i = 3-1; i > 0; i--) {
+        remain[i-1] = remain[i] * dim[i] ;
+    }
 
-    int address = 0, cummuDimention = 1 ;
-    for (int i = 2; i >= 0; i--) {  // O(n) rather than O(n^2)
-        address += (index[i] * cummuDimention) ;
-        cummuDimention *= dim[i] ;
+    // O(n)
+    int address = 0 ;
+    for (int i = 0; i < 3; i++) {
+        address += (index[i] * remain[i]) ;
     }
     cout << address << endl ;
 }
